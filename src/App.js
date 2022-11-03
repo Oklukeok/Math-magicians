3ea24d97eef5ec718b6c62afc1331b5452a49a8f
Oklukeok <lucasabermudez@gmail.com>
2022-11-03 09:43:47 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Machine from './Components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
+  render() {
+    return <Machine />;
+  }
 }
 
 export default App;
